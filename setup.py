015d1741f7de7bc229e6a4c20c3a0a860b5d8001
Jean-François Roche <jfroche@affinitic.be>
2010-03-26 13:53:27 +0100

    create simple package with paster

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='extragen',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      package_dir={'': 'src'},
+      packages=find_packages('src', exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+      ])
